c92c812531c32d69a6d5355ae6b314bf62faa171
FedericoCarboni <fr3ddydev@gmail.com>
2020-10-31 11:50:12 +0100

    fix: update install script

--- install.cjs
+++ install.cjs
@@ -1,42 +1,35 @@
 'use strict';
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+const pkg = require('./package.json');
+const assert = require('assert');
 
 const GITHUB_REPO = 'https://github.com/FedericoCarboni/node-ntsuspend';
 
+const fetch = (input) => {
+  return new Promise((resolve) => {
+    https.request(input, (res) => {
+      if (res.statusCode === 302) {
+        resolve(fetch(res.headers.location));
+      } else {
+        resolve(res);
+      }
+    }).end();
+  });
+};
+
 const install = async () => {
-  const https = require('https');
-  const path = require('path');
-  const fs = require('fs');
-  const url = require('url');
-  const pkg = require('./package.json');
-
-  const fetch = (input) => {
-    const { hostname, port, pathname, search } = new url.URL(input);
-    const options = {
-      hostname,
-      port: port || '443',
-      path: pathname + search,
-      method: 'GET',
-    };
-    return new Promise((resolve) => {
-      const req = https.request(options, (res) => {
-        if (res.statusCode === 302) {
-          resolve(fetch(res.headers.location));
-        } else {
-          resolve(res);
-        }
-      });
-      req.end();
-    });
-  };
-
   const filename = `win32-${process.arch}_lib.node`;
   const download = `${GITHUB_REPO}/releases/download/v${pkg.version}/${filename}`;
   const dest = path.join(__dirname, filename);
   const response = await fetch(download);
-  console.log(`NtSuspend Native Addon found at ${download}`);
   const stream = fs.createWriteStream(dest);
   stream.once('close', () => {
-    console.log(`NtSuspend Native Addon downloaded to ${path.relative('.', dest)}`);
+    const ntsuspend = require('./lib.cjs');
+    assert.strictEqual(typeof ntsuspend.suspend, 'function');
+    assert.strictEqual(typeof ntsuspend.resume, 'function');
+    console.log(`Success: '${dest}' installed from remote '${download}'`);
   });
   response.pipe(stream);
 };
