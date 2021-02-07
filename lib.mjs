74e231eb81b2327c73a1546556afa1ab46aea599
FedericoCarboni <fr3ddydev@gmail.com>
2021-02-07 11:31:29 +0100

    fix: add better support for module bundlers

--- lib.mjs
+++ lib.mjs
@@ -1,12 +1,12 @@
-import { createRequire } from 'module';
+import _module from 'module';
 
-let suspend = void 0;
-let resume = void 0;
+let suspend;
+let resume;
 
 if (process.platform === 'win32') {
-  const lib = createRequire(import.meta.url)(`./win32-${process.arch}_lib.node`);
-  suspend = lib.suspend;
-  resume = lib.resume;
+  const ntsuspend = (typeof require === 'function' ? require : _module.createRequire(import.meta.url))(`./win32-${process.arch}_lib.node`);
+  suspend = ntsuspend.suspend;
+  resume = ntsuspend.resume;
 }
 
 export { suspend, resume };
